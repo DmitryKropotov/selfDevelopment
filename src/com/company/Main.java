ded098daffd951962957023147c30e81ac57c45d
DKropotov <kropotovd@gmail.com>
2022-11-02 20:48:55 +0300

    merge two sorted arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,7 +20,29 @@ public class Main {
         System.out.println(cache.contains("second"));//true
         Thread.sleep(800);
         System.out.println(cache.contains("second"));//false
+        System.out.println();
 
+        int a[] = {1, 3, 4, 7};
+        int b[] = {2, 4, 6, 8, 10};
+        int result[] = mergeSortedArrays(a, b);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
 
+    private static int[] mergeSortedArrays(int[] a, int[] b) {
+        int[] result = new int[a.length+b.length];
+        int indexA = 0;
+        int indexB = 0;
+        for (int i = 0; i<result.length; i++) {
+            if (indexA<a.length && a[indexA]<=b[indexB]) {
+               result[i] = a[indexA];
+               indexA++;
+            } else {
+                result[i] = b[indexB];
+                indexB++;
+            }
+        }
+        return result;
     }
 }
